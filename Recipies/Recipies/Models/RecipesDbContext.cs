eec5ea554720e03369d45806843449ab18fe1f2f
Ka Delanegri <88409769+KaDelanegri@users.noreply.github.com>
2022-05-30 14:29:27 -0300

    Add files via upload

---
+++ Recipies/Recipies/Models/RecipesDbContext.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * using Microsoft.EntityFrameworkCore;
+
+namespace TopsyTurvyCakes.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+            this.EnsureSeedData();
+        }
+    }
+}
+*/
